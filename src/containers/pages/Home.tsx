b58881d22c0b0d16e00850e8cdbd5b8d4d856f0b
azuminowasabi <justtheperson0506@yahoo.co.jp>
2021-04-21 00:24:07 +0900

    写経用初期コード

--- src/containers/pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-import { VFC } from 'react';
-
-import Home from 'components/pages/Home';
-import orgCodes from 'data/org-codes';
-
-const EnhancedHome: VFC = () => <Home orgCodes={orgCodes} />;
-
-export default EnhancedHome;
