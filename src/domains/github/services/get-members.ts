ff33f7494f3170a06e7353fef9a7f45badc38b30
azuminowasabi <justtheperson0506@yahoo.co.jp>
2021-04-23 00:29:40 +0900

    index.tsx, get-members.tsを作成

---
+++ src/domains/github/services/get-members.ts
@@ -0,0 +1,24 @@
+import { getMembers } from 'domains/github';
+import { DEFAULT_API_OPTIONS } from './config';
+import { isUsers, User } from './../models/user';
+import ky, { Options } from 'ky';
+
+const getMembers = async (
+  orgCode: string,
+  options?: Options,
+): Promise<User[]> => {
+  const mergedOptions = {
+    ...DEFAULT_API_OPTIONS,
+    ...options,
+  };
+  const response = await ky.get(`orgs/${orgCode}/members`, mergedOptions);
+  const members = (await response.json()) as unknown[];
+
+  if (!isUsers(members)) {
+    throw Error('API type error');
+  }
+
+  return members;
+};
+
+export default getMembers;
---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import App from 'App';
+import reportWebVitals from 'reportWebVitals';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      suspense: true,
+    },
+    mutations: {
+      retry: 0,
+    },
+  },
+});
+
+ReactDOM.render(
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      {process.env.NODE_ENV === 'development' && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+    </QueryClientProvider>
+  </BrowserRouter>,
+  document.getElementById('root') as HTMLElement,
+);
+
+reportWebVitals();
